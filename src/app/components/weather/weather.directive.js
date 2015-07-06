3c02e08fd726c00b18f8af28b072194c435026f7
Nate <nate@natefonseka.com>
2015-07-06 19:12:45 -0400

    weather directive done

--- src/app/components/weather/weather.directive.js
+++ src/app/components/weather/weather.directive.js
@@ -8,9 +8,26 @@
       scope:{
         city: '@'
       },
-      controller: function($log, $http, $scope) {
-        $log.info($scope.city);
+      controller: function($log, $http, WEATHER_API) {
+        var vm = this;
+        var url = WEATHER_API + vm.city;
+        vm.temperature = 0;
+        vm.rain = false; //should i bring an umbrella?
+        $http.get(url).then(function(response) {
+          var data = response.data;
+          for (var i = 0; i < 4; i += 1) { //the list is every 3 hours, so 3 x 4 = 12 hour time
+            vm.temperature += data.list[i].main.temp;
+            for (var j = 0; j < data.list[i].weather.length; j +=1) {
+              if(data.list[i].weather[j].main === 'Rain') {
+                vm.rain = true;
+              }
+            }
+          };
+          vm.temperature = Math.round(vm.temperature / 4); //get average
+        });
       },
+      controllerAs: 'vm',
+      bindToController: true,
       templateUrl: '/app/components/weather/weather.html'
     };
   }
--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,5 +6,5 @@
     .constant('CITY', 'Philadelphia')
     .constant('NEWS', '')
     .constant('TIME', '')
-    .constant('WEATHER_API', 'api.openweathermap.org/data/2.5/forecast?units=imperial&q=');
+    .constant('WEATHER_API', 'http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=');
 })();

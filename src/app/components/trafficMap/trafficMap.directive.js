da77ae627b1495b198e896247b2dac670a467faf
Nate Fonseka <nathal.fonseka@thomsonreuters.com>
2015-12-28 11:19:10 -0500

    added a way to refresh the traffic map every 1 hr

--- src/app/components/trafficMap/trafficMap.directive.js
+++ src/app/components/trafficMap/trafficMap.directive.js
@@ -13,16 +13,27 @@
     };
 
     /** @ngInject */
-    function trafficController($http,CITY) {
+    function trafficController($http,CITY,$timeout,$interval) {
       var vm = this;
+      vm.map = {center: {latitude: 0, longitude: 0 }, zoom: 11 };
       $http.get('//maps.googleapis.com/maps/api/geocode/json?address='+CITY).success(function(data){
         var location = data.results[0].geometry.location;
         vm.map.center.latitude = location.lat;
         vm.map.center.longitude = location.lng;
         vm.show = true;
+        vm.showMapLayer = true;
+        $interval(refreshMap, 1000 * 60 * 60);
       });
-      vm.map = {center: {latitude: 0, longitude: 0 }, zoom: 12 };
       vm.show = false;
+      vm.showMapLayer = false;
+
+      function refreshMap() {
+        vm.showMapLayer = false;
+        $timeout(function() {
+          vm.showMapLayer = true;
+        }, 500, 1);
+      }
+
     }
   }
 })();

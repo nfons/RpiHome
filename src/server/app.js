99b8f0c2485a9a044883b00a318a7050c7fcb1b9
Nate Fonseka <nathal.fonseka@thomsonreuters.com>
2015-12-30 11:24:53 -0500

    making it so that express server will serve up the dist dir as well

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var dinnerRoutes = require('./routes/dinner-route');
 
@@ -11,6 +12,10 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use('/dinner', dinnerRoutes);
+
+//serve static files
+app.use('/',express.static(path.resolve(__dirname+'../../../dist')));
+
 
 mongoose.connect('mongodb://localhost:27017/rpihome');
 
@@ -22,5 +27,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s, %s', host, port, __dirname);
 });
